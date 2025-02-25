791b23c9753c914a05813b1dc06f4088e3abbee5
sajo13 <sajosunny007@gmail.com>
2025-02-25 16:44:29 +0530

    Create Index and Body
    - The index parameter is used to specify the name of the index where you want to store the document.
    - The body parameter contains the actual document data that you want to index.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use MailerLite\LaravelElasticsearch\Facade as Elasticsearch;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $data = [
+        'index' => 'my_index',  // The name of the index
+        'body'  => [            // The document data to be stored
+                'testField' => 'abc'
+        ]
+    ];
+    
+    $return = Elasticsearch::index($data);
+
+    dd($return);
+
+    // return view('welcome');
 });
