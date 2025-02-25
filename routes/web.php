af297f34a0c4d49f8795e8a1e789e960215c0d1a
sajo13 <sajosunny007@gmail.com>
2025-02-25 17:12:37 +0530

    Fetch Index Data based on Id

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,24 @@ use MailerLite\LaravelElasticsearch\Facade as Elasticsearch;
 
 Route::get('/', function () {
 
-    $data = [
-        'index' => 'my_index',  // The name of the index
-        'body'  => [            // The document data to be stored
-                'testField' => 'abc'
-        ]
-    ];
+    // $data = [
+    //     'index' => 'my_index',  // The name of the index
+    //     'body'  => [            // The document data to be stored
+    //             'testField' => 'abc'
+    //     ],
+    //     'id' => 'my_id',
+    // ];
     
-    $return = Elasticsearch::index($data);
+    // $return = Elasticsearch::index($data);
 
-    dd($return);
+    $params = [
+        'index' => 'my_index',
+        'id'    => 'my_id'
+    ];
+
+    // Get doc at /my_index/_doc/my_id
+    $return = Elasticsearch::get($params);
+    dd($return['_source']);
 
     // return view('welcome');
 });
